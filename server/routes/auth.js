53ea023b1cfdd86fc7fe56076e95dcf81c8179b8
kitkanaporn umpaimungkorn <kitkanaporn8125@gmail.com>
2025-11-21 16:27:11 +0700

    fix some mistake of this code to ready to run dev

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,11 @@
-const {express} = require("express") ;
+const express = require("express") ;
 const router = express.Router() ;
 const {protect , authorize} = require("../middleware/auth") ;
-const {register , login , logout , getme} = require("../controllers/auth")
+const {register , login , logout , getMe} = require("../controllers/auth")
 
 router.post('/register' , register) ;
 router.post('/login' , login) ;
-router.get('/me' , protect , getme) ;
+router.get('/me' , protect , getMe) ;
 router.get('/logout' , logout) ;
 
 module.exports = router ;
