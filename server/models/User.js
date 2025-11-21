be9f9f6cf0085d8d5cd66e232c9202b7c979a4fc
kitkanaporn umpaimungkorn <kitkanaporn8125@gmail.com>
2025-11-21 17:26:19 +0700

    second fix that is add telphoneNumber to user and auth

--- server/models/User.js
+++ server/models/User.js
@@ -1,3 +1,4 @@
+const { request } = require('http');
 const mongoose = require('mongoose') ;
 
 const UserSchema = new mongoose.Schema({
@@ -7,7 +8,13 @@ const UserSchema = new mongoose.Schema({
         required : [true , "please add user name" ] ,
         trim : true ,
         maxlength : [100 , "user name can not be more than 100 characters" ]
-    } , 
+    } ,
+    
+    telphoneNumber : {
+        type : String ,
+        maxlength : [10 , 'please add user telephone can not more than 10 charecters'] ,
+        required : [true , "Please add user telephone number" ],
+    } ,
 
     email : {
         type : String ,
