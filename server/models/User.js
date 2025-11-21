1fbd41e3a368e5983c166bdad6bba80bff05557b
U.Kitkanaporn <6630014521@student.chula.ac.th>
2025-11-21 23:19:04 +0700

    Revert "Fix"

--- server/models/User.js
+++ server/models/User.js
@@ -20,7 +20,7 @@ const UserSchema = new mongoose.Schema({
     email : {
         type : String ,
         required : [true , "please add user email" ] ,
-        unique : [true , "please use new email. this email already used"] ,
+        unique : true ,
         match : [
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
             'Please add a valid email'
